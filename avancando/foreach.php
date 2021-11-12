e1a52c46738af939e1d52ba7aaea1c0f14a86c62
EdisonJuniorDesigner <mage1232010@gmail.com>
2021-11-11 22:56:27 -0300

    atualizando e adicionando informações

--- avancando/foreach.php
+++ avancando/foreach.php
@@ -1,21 +1,26 @@
 <?php
 
 $contasCorrentes = [
-    12345678910 => [
+    '123.456.789-10' => [
         'titular' => 'Edison',
         'saldo' => 1000
     ],
-    12345678911 => [
+    '123.456.789-11' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    12375678910 => [
+    '123.756.789-10' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ]
 ];
 
+$contasCorrentes['125.854.123-85'] = [
+    'titular' => 'Claudia',
+    'saldo' => 2000
+];
+
 // foreach / Para cada
 foreach($contasCorrentes as $cpf => $conta){
-    echo $cpf.PHP_EOL;
+    echo $cpf." ". $conta['titular'].PHP_EOL;
 }
--- avancando/listas.php
+++ avancando/listas.php
@@ -1,6 +1,12 @@
 <?php
 
 $idadeList = [21, 23, 19, 25, 30, 41, 18];
-$umaIdade = $idadeList[4];
 
-echo $umaIdade;
+// por padrão quando deixamos o array vazio o
+// PHP adiciona o item no final do array que no
+// caso vai adicionar o '20' ao lado do '18'
+$idadeList[] = 20;
+
+foreach ($idadeList as $idade){
+    echo $idade.PHP_EOL;
+}
