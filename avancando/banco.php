1fc8c36ea2398dce33e7726cbfbc90787035e4b6
EdisonJuniorDesigner <mage1232010@gmail.com>
2021-11-13 03:23:33 -0300

    require e include

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'funcoes.php';
+
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Edison',
@@ -14,33 +16,6 @@ $contasCorrentes = [
         'saldo' => 300
     ]
 ];
-
-// Uma função pode retornar um valor ou não retornar nada só executar.
-
-function sacar(array $conta, float $valorASacar) : array
-{
-    if($valorASacar > $conta['saldo']){
-        exibeMensagem("Você não pode sacar este valor");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-    return $conta;
-}
-
-function exibeMensagem(string $mensagem)
-{
-    echo $mensagem.PHP_EOL;
-}
-
-function depositar(array $conta, float $valorADepositar) : array
-{
-    if($valorADepositar > 0){
-        $conta['saldo'] += $valorADepositar;
-    } else {
-        exibeMensagem("Depósitos precisam ser positivos");
-    }
-    return $conta;
-}
 
 $contasCorrentes['123.456.789-11'] = sacar($contasCorrentes['123.456.789-11'], 500);
 $contasCorrentes['123.756.789-10'] = sacar($contasCorrentes['123.756.789-10'], 200);
